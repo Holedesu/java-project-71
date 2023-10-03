5b072068dd6d645254dee515495ebc8440d2f072
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-10-03 12:05:23 +0300

    Ended fourth step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,10 +15,17 @@ class App {
 
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 class Gendiff implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
     @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
     private String help;
     @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
     private String version;
+
     @Override
     public Integer call() {
         return 0;
